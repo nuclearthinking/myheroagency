6af533b015886b1aea52fed95b1f59e09fb1dd44
nuclearthinking <onifent@gmail.com>
2016-05-08 11:23:42 +0300

    Revert " GameStateManager вариант" (#14)

--- core/src/com/nuclearthinking/myheroagency/Main.java
+++ core/src/com/nuclearthinking/myheroagency/Main.java
@@ -2,17 +2,27 @@ package com.nuclearthinking.myheroagency;
 
 import com.badlogic.gdx.Game;
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 
 public class Main extends Game {
-    GameStateManager gsm;
+    SpriteBatch batch;
+    Texture img;
+
     @Override
     public void create() {
-      gsm =   new GameStateManager();
+        batch = new SpriteBatch();
+        img = new Texture("badlogic.jpg");
+
     }
 
     @Override
     public void render() {
-        gsm.update(Gdx.graphics.getDeltaTime());
-        gsm.render();
+        Gdx.gl.glClearColor(1, 0, 0, 1);
+        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+        batch.begin();
+        batch.draw(img, 0, 0);
+        batch.end();
     }
 }
--- core/src/com/nuclearthinking/myheroagency/view/BattleScreen.java
+++ core/src/com/nuclearthinking/myheroagency/view/BattleScreen.java
@@ -1,55 +1,12 @@
 package com.nuclearthinking.myheroagency.view;
 
-import com.badlogic.gdx.Gdx;
-import com.badlogic.gdx.graphics.GL20;
-import com.nuclearthinking.myheroagency.State;
-import com.nuclearthinking.myheroagency.GameStateManager;
-import com.nuclearthinking.myheroagency.ui.FontFactory;
-
-import static com.badlogic.gdx.Input.Keys;
-
 /**
  * Date: 05.05.2016
  * Time: 7:01
  *
  * @author Vladislav Radchenko (onfient@gmail.com)
  */
-public class BattleScreen extends State {
+public class BattleScreen extends AbstractScreen
+{
 
-
-    public BattleScreen(GameStateManager gsm) {
-        super(gsm);
-    }
-
-    @Override
-    public void handleKeyboard(int key) {
-        if (key == Keys.Q) {
-            gsm.setState(GameStateManager.GameState.BATTLE);
-        } else if (key == Keys.W) {
-            gsm.setState(GameStateManager.GameState.HOME);
-        } else if (key == Keys.E) {
-            gsm.setState(GameStateManager.GameState.START);
-        } else if (key == Keys.R) {
-            gsm.setState(GameStateManager.GameState.QUEST);
-        }
-    }
-
-    @Override
-    public void update(float dt) {
-
-    }
-
-    @Override
-    public void render() {
-        Gdx.gl.glClearColor(0, 0, 0, 1);
-        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-        spriteBatch.begin();
-        FontFactory.getFont9().draw(spriteBatch, "Battle screen", 20, 20);
-        spriteBatch.end();
-    }
-
-    @Override
-    public void dispose() {
-
-    }
 }
