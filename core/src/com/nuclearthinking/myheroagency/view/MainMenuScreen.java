d32031ff743c5660959f71e446abeac164af539b
Izonami <kuokatsu@gmail.com>
2016-05-12 18:08:45 +0700

    Попробовал другой вариант создания кнопок. Засунул кнопки в таблицу

--- core/src/com/nuclearthinking/myheroagency/view/MainMenuScreen.java
+++ core/src/com/nuclearthinking/myheroagency/view/MainMenuScreen.java
@@ -2,25 +2,24 @@ package com.nuclearthinking.myheroagency.view;
 
 import com.badlogic.gdx.audio.Music;
 import com.badlogic.gdx.graphics.g2d.TextureAtlas;
-import com.badlogic.gdx.math.Interpolation;
-import com.badlogic.gdx.scenes.scene2d.Actor;
-import com.badlogic.gdx.scenes.scene2d.InputEvent;
 import com.badlogic.gdx.scenes.scene2d.ui.Image;
 import com.badlogic.gdx.scenes.scene2d.ui.Skin;
-import com.badlogic.gdx.scenes.scene2d.ui.TextButton;
-import com.badlogic.gdx.scenes.scene2d.utils.ClickListener;
+import com.badlogic.gdx.scenes.scene2d.ui.Table;
 import com.nuclearthinking.myheroagency.controller.Assets;
+import com.nuclearthinking.myheroagency.ui.MenuButton;
 
-import static com.badlogic.gdx.scenes.scene2d.actions.Actions.*;
+import static com.badlogic.gdx.scenes.scene2d.actions.Actions.moveTo;
+import static com.badlogic.gdx.scenes.scene2d.actions.Actions.sequence;
 
 /**
  * Created by Izonami on 11.05.2016.
  */
 public class MainMenuScreen extends AbstractScreen {
 
+    private Table table;
     private Image bgMenu;
     private Music menuMusic;
-    private TextButton play, load, exit;
+    private MenuButton play, load, exit;
 
     @Override
     public void buildStage() {
@@ -30,44 +29,37 @@ public class MainMenuScreen extends AbstractScreen {
                 .getAssetManager()
                 .get("ui/ui.atlas", TextureAtlas.class));
 
+        table = new Table();
+        table.setFillParent(true);
+        table.setDebug(true);
+        table.setClip(true);
+
         initButton();
 
-        addActor(play);
-        addActor(load);
-        addActor(exit);
+        table.add(play).width(100).pad(10);
+        table.row();
+        table.add(load).width(100).pad(10);
+        table.row();
+        table.add(exit).width(100).pad(10);
+
+        addActor(table);
+        addAction(sequence(moveTo(getWidth(), 0), moveTo(0, 0, .5f)));
     }
 
     //TODO: Продумать правильное решение для добавление кнопок. Какая нибудь фабрика или иное решение. Все что ниже сплошной говнокод
     private void initButton(){
-        play = setUpButton("Play", 90, 260, 150,35);
-        play.addAction(sequence(alpha(0), parallel(fadeIn(.5f), moveBy(0, -20, .5f, Interpolation.pow5Out))));
-        play.addListener(new ClickListener()
-        {
-            @Override
-            public void enter(InputEvent event, float x, float y, int pointer, Actor fromActor)
-            {
-                play.addAction(sequence(alpha(0), parallel(fadeIn(.4f))));
-            }
-
-            @Override
-            public void clicked(InputEvent event, float x, float y)
-            {
-
-            }
-        });
-
-        load = setUpButton("Loading", 250, 260, 150,35);
-        load.addAction(sequence(alpha(0), parallel(fadeIn(.5f), moveBy(0, -20, .5f, Interpolation.pow5Out))));
-        exit = setUpButton("Exit", 450, 260, 150,35);
-        exit.addAction(sequence(alpha(0), parallel(fadeIn(.5f), moveBy(0, -20, .5f, Interpolation.pow5Out))));
+        play = new MenuButton("Play", Assets.getInstance().getSkin());
+        play.setSize(150, 35);
+        load = new MenuButton("Loading", Assets.getInstance().getSkin());
+        load.setSize(150, 35);
+        exit = new MenuButton("Exit", Assets.getInstance().getSkin());
+        exit.setSize(150, 35);
     }
 
-    private TextButton setUpButton(String label, int posX, int posY, int sizeX, int sizeY)
-    {
-        TextButton button = new TextButton(label, Assets.getInstance().getSkin(), "default");
-        button.setPosition(posX, posY);
-        button.setSize(sizeX, sizeY);
+    @Override
+    public void resize(int width, int height){
+        super.resize(width, height);
 
-        return button;
+        table.setSize(width, height);
     }
 }
