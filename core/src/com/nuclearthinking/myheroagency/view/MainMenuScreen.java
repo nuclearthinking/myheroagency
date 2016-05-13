79cee690807f7e04352c6b2d223bb7f45687db69
Izonami <kuokatsu@gmail.com>
2016-05-13 10:06:31 +0700

    Переделал создание кнопок

--- core/src/com/nuclearthinking/myheroagency/view/MainMenuScreen.java
+++ core/src/com/nuclearthinking/myheroagency/view/MainMenuScreen.java
@@ -1,15 +1,18 @@
 package com.nuclearthinking.myheroagency.view;
 
+import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.audio.Music;
 import com.badlogic.gdx.graphics.g2d.TextureAtlas;
+import com.badlogic.gdx.scenes.scene2d.Actor;
+import com.badlogic.gdx.scenes.scene2d.InputEvent;
 import com.badlogic.gdx.scenes.scene2d.ui.Image;
 import com.badlogic.gdx.scenes.scene2d.ui.Skin;
 import com.badlogic.gdx.scenes.scene2d.ui.Table;
+import com.badlogic.gdx.scenes.scene2d.ui.TextButton;
+import com.badlogic.gdx.scenes.scene2d.utils.ClickListener;
 import com.nuclearthinking.myheroagency.controller.Assets;
-import com.nuclearthinking.myheroagency.ui.MenuButton;
 
-import static com.badlogic.gdx.scenes.scene2d.actions.Actions.moveTo;
-import static com.badlogic.gdx.scenes.scene2d.actions.Actions.sequence;
+import static com.badlogic.gdx.scenes.scene2d.actions.Actions.*;
 
 /**
  * Created by Izonami on 11.05.2016.
@@ -19,7 +22,8 @@ public class MainMenuScreen extends AbstractScreen {
     private Table table;
     private Image bgMenu;
     private Music menuMusic;
-    private MenuButton play, load, exit;
+    private TextButton play, load, exit;
+    private static ClickListener button;
 
     @Override
     public void buildStage() {
@@ -34,6 +38,7 @@ public class MainMenuScreen extends AbstractScreen {
         table.setDebug(true);
         table.setClip(true);
 
+        createListener();
         initButton();
 
         table.add(play).width(100).pad(10);
@@ -48,12 +53,15 @@ public class MainMenuScreen extends AbstractScreen {
 
     //TODO: Продумать правильное решение для добавление кнопок. Какая нибудь фабрика или иное решение. Все что ниже сплошной говнокод
     private void initButton(){
-        play = new MenuButton("Play", Assets.getInstance().getSkin());
+        play = new TextButton("Play", Assets.getInstance().getSkin(), "default");
         play.setSize(150, 35);
-        load = new MenuButton("Loading", Assets.getInstance().getSkin());
+        play.addListener(button);
+        load = new TextButton("Loading", Assets.getInstance().getSkin(), "default");
         load.setSize(150, 35);
-        exit = new MenuButton("Exit", Assets.getInstance().getSkin());
+        load.addListener(button);
+        exit = new TextButton("Exit", Assets.getInstance().getSkin(), "default");
         exit.setSize(150, 35);
+        exit.addListener(button);
     }
 
     @Override
@@ -62,4 +70,35 @@ public class MainMenuScreen extends AbstractScreen {
 
         table.setSize(width, height);
     }
+
+    private void createListener(){
+        button = new ClickListener(){
+            @Override
+            public void clicked(InputEvent event, float x, float y) {
+                if(event.getListenerActor() == play){
+
+                }else if(event.getListenerActor() == load){
+
+                }else if(event.getListenerActor() == exit){
+                    Gdx.app.exit();
+                }else {
+                    System.out.println("I don't know");
+                }
+            }
+
+            @Override
+            public void enter(InputEvent event, float x, float y, int pointer, Actor fromActor) {
+                if(event.getListenerActor() == play){
+                    play.addAction(sequence(alpha(0), parallel(fadeIn(.4f))));
+                }else if(event.getListenerActor() == load){
+                    load.addAction(sequence(alpha(0), parallel(fadeIn(.4f))));
+                }else if(event.getListenerActor() == exit){
+                    exit.addAction(sequence(alpha(0), parallel(fadeIn(.4f))));
+                }else {
+                    System.out.println("I don't know");
+                }
+            }
+        };
+    }
+
 }
