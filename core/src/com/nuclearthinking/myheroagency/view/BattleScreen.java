1089c53f06440acdf4eb9cfbbb811fa31e380add
Vladislav Radchenko <onifent@gmail.com>
2016-05-22 10:03:48 +0300

    пытаюсь сделать точный клик листнер для спрайтов

--- core/src/com/nuclearthinking/myheroagency/view/BattleScreen.java
+++ core/src/com/nuclearthinking/myheroagency/view/BattleScreen.java
@@ -1,7 +1,12 @@
 package com.nuclearthinking.myheroagency.view;
 
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.OrthographicCamera;
+import com.badlogic.gdx.graphics.Pixmap;
 import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.graphics.g2d.TextureAtlas;
+import com.badlogic.gdx.math.Vector3;
 import com.badlogic.gdx.scenes.scene2d.InputEvent;
 import com.badlogic.gdx.scenes.scene2d.ui.Image;
 import com.badlogic.gdx.scenes.scene2d.ui.Skin;
@@ -26,7 +31,9 @@ public class BattleScreen extends AbstractScreen {
     Table table;
     TextButton defend, attack, done;
     Image backgroundImage, manImage;
-    Texture backgroundTexture, manTexture;
+    Texture backgroundTexture, manTexture, castleTexture;
+    Sprite sprite, castle;
+    OrthographicCamera camera;
 
     public BattleScreen(GameData gameData) {
         this.gameData = gameData;
@@ -40,7 +47,8 @@ public class BattleScreen extends AbstractScreen {
         done = createButton("doneButtonLabel", .7f, skin, "kramola");
         manTexture = asset.get("img/man.png");
         backgroundTexture = asset.get("img/battleBackground.png");
-
+        castleTexture = asset.get("img/splash.png");
+        camera = new OrthographicCamera(Constants.GAME_W, Constants.GAME_H);
     }
 
     @Override
@@ -51,16 +59,19 @@ public class BattleScreen extends AbstractScreen {
         table.setDebug(Constants.DEBUG);
         table.left().bottom();
         table.row();
-        table.add(attack).left().bottom().width(100).height(40).pad(10).fill(false);
-        table.add(defend).left().bottom().width(100).height(40).pad(10).fill(false);
-        table.add(done).right().bottom().width(100).height(40).pad(10).expandX();
+//        table.add(attack).left().bottom().width(100).height(40).pad(10).fill(false);
+//        table.add(defend).left().bottom().width(100).height(40).pad(10).fill(false);
+//        table.add(done).right().bottom().width(100).height(40).pad(10).expandX();
 
         stage.addActor(backgroundImage);
-        stage.addActor(manImage);
+//        stage.addActor(manImage);
         stage.addActor(table);
-        manImage.setPosition(50, 100);
-        manImage.addListener(new ManListner());
-
+//        manImage.setPosition(50, 100);
+//        manImage.addListener(new ManListner());
+        castle = new Sprite(castleTexture);
+        sprite = new Sprite(manTexture);
+        castle.setPosition(200, 200);
+        camera.setToOrtho(false);
     }
 
     private TextButton createButton(String localKey, float fontScale, Skin skin, String font) {
@@ -71,7 +82,30 @@ public class BattleScreen extends AbstractScreen {
 
     @Override
     public void render(float delta) {
+
+        camera.update();
+        stage.getBatch().setProjectionMatrix(camera.combined);
         super.render(delta);
+        stage.getBatch().begin();
+        castle.draw(stage.getBatch());
+        sprite.draw(stage.getBatch());
+        if (Gdx.input.justTouched()) {
+//            System.out.println("Toched");
+            Vector3 tochPoint = new Vector3();
+            camera.unproject(tochPoint.set(Gdx.input.getX(), Gdx.input.getY(), 0));
+
+
+            if (castle.getBoundingRectangle().contains(tochPoint.x, tochPoint.y)) {
+                System.out.println("toch on sprite");
+                castle.getTexture().getTextureData().prepare();
+                Pixmap pixmap = castle.getTexture().getTextureData().consumePixmap();
+                System.out.println("touch point " + tochPoint.toString());
+                System.out.println(pixmap.getPixel(20,20));
+                System.out.println(pixmap.getPixel((int) tochPoint.x, (int) tochPoint.y));
+            }
+        }
+        stage.getBatch().end();
+
     }
 
     @Override
