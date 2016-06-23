88f41dbd3ac267b45ae3ed007ea60a05f35d98b7
Izonami <kuokatsu@gmail.com>
2016-06-23 14:55:02 +0700

    Нужно будет подумать ещё в направлении фабрики для объектов

---
+++ core/src/com/nuclearthinking/myheroagency/controller/ObjectManager.java
@@ -0,0 +1,26 @@
+package com.nuclearthinking.myheroagency.controller;
+
+import com.badlogic.gdx.graphics.g2d.Animation;
+import com.badlogic.gdx.graphics.g2d.TextureAtlas;
+import com.nuclearthinking.myheroagency.model.Player;
+
+/**
+ * Created by Izonami on 23.06.2016.
+ */
+public class ObjectManager {
+
+    private static Player player;
+    private Animation idle, left,right;
+    private final TextureAtlas playerAtlas = Asset.getInstance().get("player/player.pack");
+
+    public Player getPlayer(){
+        if(player == null){
+            idle = new Animation(1 / 2f, playerAtlas.findRegions("still"), Animation.PlayMode.LOOP);
+            left = new Animation(1 / 6f, playerAtlas.findRegions("left"), Animation.PlayMode.LOOP);
+            right = new Animation(1 / 6f, playerAtlas.findRegions("right"), Animation.PlayMode.LOOP);
+            player = new Player(null, 40, 35, idle,left,right);
+            return player;
+        }
+        return player;
+    }
+}
--- core/src/com/nuclearthinking/myheroagency/view/HomeScreen.java
+++ core/src/com/nuclearthinking/myheroagency/view/HomeScreen.java
@@ -1,16 +1,13 @@
 package com.nuclearthinking.myheroagency.view;
 
 import com.badlogic.gdx.graphics.OrthographicCamera;
-import com.badlogic.gdx.graphics.g2d.Animation;
-import com.badlogic.gdx.graphics.g2d.TextureAtlas;
-import com.nuclearthinking.myheroagency.controller.Asset;
 import com.nuclearthinking.myheroagency.controller.LayerController;
+import com.nuclearthinking.myheroagency.controller.ObjectManager;
 import com.nuclearthinking.myheroagency.controller.PlayerController;
 import com.nuclearthinking.myheroagency.controller.SpriteManager;
 import com.nuclearthinking.myheroagency.model.GameData;
 import com.nuclearthinking.myheroagency.model.GameObject;
 import com.nuclearthinking.myheroagency.model.MapManager;
-import com.nuclearthinking.myheroagency.model.Player;
 import com.nuclearthinking.myheroagency.ui.hud.HudGame;
 
 
@@ -26,7 +23,7 @@ public class HomeScreen extends AbstractScreen {
     private MapManager manager;
     private LayerController layerController;
     private PlayerController playerController;
-    private Player player;
+    private ObjectManager object;
 
     public HomeScreen() {
         this(new GameData());
@@ -36,12 +33,14 @@ public class HomeScreen extends AbstractScreen {
         this.gameData = gameData;
 
         //TODO: Думаю стоит для этого сделать какую нибудь фабрику, что бы каждый раз не создавать
-        final TextureAtlas playerAtlas = Asset.getInstance().get("player/player.pack");
+        /*final TextureAtlas playerAtlas = Asset.getInstance().get("player/player.pack");
         Animation idle, left,right;
         idle = new Animation(1 / 2f, playerAtlas.findRegions("still"), Animation.PlayMode.LOOP);
         left = new Animation(1 / 6f, playerAtlas.findRegions("left"), Animation.PlayMode.LOOP);
         right = new Animation(1 / 6f, playerAtlas.findRegions("right"), Animation.PlayMode.LOOP);
-        player = new Player(null, 40, 35, idle,left,right);
+        player = new Player(null, 40, 35, idle,left,right);*/
+
+        object = new ObjectManager();
     }
 
     @Override
@@ -49,10 +48,9 @@ public class HomeScreen extends AbstractScreen {
         hudGame = new HudGame(stage.getBatch());
         manager = new MapManager();
         layerController = new LayerController(hudGame);
-        playerController = new PlayerController(player);
-
-        player.setPosition(1000, 3000);
-        SpriteManager.addGameObject(player);
+        playerController = new PlayerController(object.getPlayer());
+        object.getPlayer().setPosition(1000,3000);
+        SpriteManager.addGameObject(object.getPlayer());
 
         multi.addProcessor(hudGame.getHudStage());
         multi.addProcessor(playerController);
@@ -65,7 +63,7 @@ public class HomeScreen extends AbstractScreen {
         manager.getRenderer().setView((OrthographicCamera) stage.getCamera());
         stage.getBatch().setProjectionMatrix(hudGame.getHudCamera().combined);
 
-        stage.getCamera().position.set(player.getX(), player.getY(), 0);
+        stage.getCamera().position.set(object.getPlayer().getX(), object.getPlayer().getY(), 0);
         layerController.update();
         playerController.update();
 
